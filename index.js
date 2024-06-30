14d99716bd7fd6c07ba156902302865e99f24933
Nmsviper93 <133047743+Nmsviper93@users.noreply.github.com>
2024-06-30 16:14:09 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'Capstone2_App.css';
+import App from 'Cappstone2_App.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
